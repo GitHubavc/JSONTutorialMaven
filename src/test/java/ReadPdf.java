b54f7d80c09e735117db456e380278f396b004b0
amitGitHubavc <amitvchandran@gmail.com>
2025-02-17 11:21:31 +0530

    Pdffile

---
+++ src/test/java/ReadPdf.java
@@ -0,0 +1,21 @@
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ReadPdf {
+    static String pdfPath="/Users/amit/IdeaProjects/JSONTutorialMaven/Data/sample.pdf";
+
+    public static void main(String[] args) throws IOException {
+        File pdfFile=new File(pdfPath);
+        PDDocument doc=Loader.loadPDF(pdfFile);
+
+        PDFTextStripper pdfStripper= new PDFTextStripper();
+        String text=pdfStripper.getText(doc);
+        doc.close();
+        System.out.println(text);
+
+    }
+}
